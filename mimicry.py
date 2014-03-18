3000bdb73a08cf13360eaca44e8745b7e7718289
Sebastian Ullrich <sebasti@nullri.ch>
2014-03-18 22:20:59 +0100

    Wrap dumb AST classes to at least give them value semantics.
    Let's see if this is a good idea.

--- mimicry.py
+++ mimicry.py
@@ -33,8 +33,65 @@ def windows(values, n=2):
         values = values[1:]
         yield window
 
+def wrap(value):
+    if isinstance(value, ast.AST) and not isinstance(value, ASTWrapper):
+        return ASTWrapper(value)
+    elif isinstance(value, list):
+        return map(wrap, value)
+    else:
+        return value
+
+class ASTWrapper(ast.AST):
+    """ast.AST wrapper for more Pythonicity."""
+    def __init__(self, node):
+        object.__setattr__(self, '_node', node)
+        self._fields = self._node._fields
+
+    def __getattr__(self, name):
+        value = getattr(self._node, name)
+        setattr(self, name, value)
+        return getattr(self, name)
+
+    def __setattr__(self, name, value):
+        object.__setattr__(self, name, wrap(value))
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return equal_node_structure([self, other]) and \
+                all(n1 == n2 for n1, n2 in
+                    zip(self.child_nodes, other.child_nodes))
+
+    def __repr__(self):
+        # shamelessly copied from ast.dump
+        def _format(node):
+            if isinstance(node, AST):
+                fields = [(a, _format(b)) for a, b in ast.iter_fields(node)]
+                return '%s(%s)' % (node.type.__name__, ', '.join(
+                    ('%s=%s' % field for field in fields)
+                ))
+            elif isinstance(node, list):
+                return '[%s]' % ', '.join(_format(x) for x in node)
+            return repr(node)
+        return _format(self)
+
+    @property
+    def child_nodes(self):
+        return list(ast.iter_child_nodes(self))
+
+    @property
+    def type(self):
+        return type(self._node)
+
+    def of_class(self, class_):
+        return self.type == class_
+
+AST = ASTWrapper
+
+def parse(source):
+    return ASTWrapper(ast.parse(source))
+
 def parse_expr(source):
-    return ast.parse(source).body[0].value
+    return parse(source).body[0].value
 
 def parse_exprs(*sources):
     return [parse_expr(source) for source in sources]
@@ -51,7 +108,7 @@ def equal_node_structure(nodes):
     False
     """
     def equal(n1, n2):
-        if n1.__class__ != n2.__class__:
+        if n1.type != n2.type:
             return False
 
         for field in n1._fields:
@@ -60,7 +117,7 @@ def equal_node_structure(nodes):
             if isinstance(n1_val, list):
                 if len(n1_val) != len(n2_val):
                     return False
-            elif not isinstance(n1_val, ast.AST):
+            elif not isinstance(n1_val, AST):
                 if n1_val != n2_val:
                     return False
 
@@ -68,13 +125,6 @@ def equal_node_structure(nodes):
 
     nodes = list(nodes)
     return len(nodes) < 2 or all(equal(nodes[0], n) for n in nodes[1:])
-
-def nodes_equal(n1, n2):
-    return equal_node_structure([n1, n2]) and \
-        all(nodes_equal(n1, n2) for n1, n2 in zip(
-            list(ast.iter_child_nodes(n1)),
-            list(ast.iter_child_nodes(n2))
-        ))
 
 def find_change_context(old, new):
     """Finds the smallest context of a change.
@@ -87,17 +137,14 @@ def find_change_context(old, new):
 
     changed_pairs = filter(None, [
         find_change_context(old_child, new_child)
-        for old_child, new_child in zip(
-            list(ast.iter_child_nodes(old)),
-            list(ast.iter_child_nodes(new))
-        )
+        for old_child, new_child in zip(old.child_nodes, new.child_nodes)
     ])
     if len(changed_pairs) == 0:
         return None
     elif len(changed_pairs) == 1:
         ctx = changed_pairs[0]
         # prefer Call contexts
-        if isinstance(old, ast.Call) and not isinstance(ctx[0], ast.Call):
+        if old.of_class(ast.Call) and not ctx[0].of_class(ast.Call):
             ctx = old, new
         return ctx
     else:
@@ -111,7 +158,7 @@ class Template(collections.namedtuple('Template', ['node', 'holes'])):
     Call(func=Name(id='f', ctx=Load()), args=[Name(id='a', ctx=Load()), Name(id='$0', ctx=None)], keywords=[], starargs=None, kwargs=None)
     """
     def __repr__(self):
-        return ast.dump(self.instantiate({
+        return repr(self.instantiate({
             hole: ast.Name('$%d' % i, None) for i, hole in enumerate(self.holes)
         }))
 
@@ -123,7 +170,7 @@ class Template(collections.namedtuple('Template', ['node', 'holes'])):
         class Transformer(ast.NodeTransformer):
             def visit(self, node):
                 if node in holes:
-                    return replacement[node]
+                    return wrap(replacement[node])
                 else:
                     return self.generic_visit(node)
 
@@ -146,14 +193,14 @@ def get_most_specific_template(nodes):
         if not equal_node_structure(nodes):
             return {node}
 
-        return union(map(get_holes, transpose(map(ast.iter_child_nodes, nodes))))
+        return union(map(get_holes, transpose([n.child_nodes for n in nodes])))
 
     return Template(nodes[0], get_holes(nodes))
 
 def main():
     proj = Project('test/')
     def get_ast(mod):
-        return proj.pycore.get_module(mod).get_ast()
+        return wrap(proj.pycore.get_module(mod).get_ast())
 
     edit_steps = ['a', 'b', 'c']
     contexts = [find_change_context(*win) for win in windows(map(get_ast, edit_steps))]
